9338a74d6a4fb24455b8b95555f71be947a3705f
konstantinvelev <kockata.velev@abv.bg>
2020-12-05 14:26:44 +0200

    Generate post service

--- FitnessBook/src/app/shared/shared.module.ts
+++ FitnessBook/src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-import { EmailValidatorDirective } from './email-validator.directive';
 import { SubmitOnValidDirective } from './dir.directive';
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    EmailValidatorDirective,
     SubmitOnValidDirective
   ],
   imports: [
@@ -15,7 +13,6 @@ import { SubmitOnValidDirective } from './dir.directive';
   ],
   exports:[
     LoaderComponent,
-    EmailValidatorDirective,
     SubmitOnValidDirective
   ]
 })
